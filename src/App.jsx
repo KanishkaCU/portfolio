976d98323233661173e1ea7305c3f48dcb62da67
KanishkaCU <kanishkacu888@gmail.com>
2026-04-28 10:39:02 +0530

    28/4

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1 style={{ padding: "100px 24px", textAlign: "center" }}>
+        Portfolio coming soon...
+      </h1>
+    </div>
+  );
+}
